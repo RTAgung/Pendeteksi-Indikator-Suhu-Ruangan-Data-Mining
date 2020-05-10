975d79d6b04e67e8c06ccf78e0039cb1c9e1d39f
Rama Tri Agung <ramatriagung91@gmail.com>
2020-05-10 10:03:54 +0700

    fixed database

--- data_mining_suhu.sql
+++ data_mining_suhu.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2020 at 10:59 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.1
+-- Generation Time: May 10, 2020 at 05:02 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `data` (
-  `id` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `RoomVolume` int(11) NOT NULL,
   `PeopleQty` int(11) NOT NULL,
   `Time` time NOT NULL,
@@ -40,6 +40,31 @@ CREATE TABLE `data` (
   `TempIndicator` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `data`
+--
+
+INSERT INTO `data` (`Id`, `RoomVolume`, `PeopleQty`, `Time`, `Month`, `Weather`, `CoolerQty`, `MASL`, `TempIndicator`) VALUES
+(3, 15, 1, '22:00:00', 12, 2, 1, 750, 1),
+(4, 24, 2, '07:00:00', 8, 2, 2, 800, 1),
+(5, 80, 12, '03:00:00', 3, 3, 4, 900, 1),
+(6, 90, 30, '06:00:00', 9, 3, 4, 850, 1),
+(7, 160, 50, '12:00:00', 5, 1, 7, 820, 1),
+(8, 165, 45, '16:00:00', 4, 1, 10, 180, 1),
+(9, 1100, 900, '18:00:00', 7, 1, 35, 400, 1),
+(10, 14, 2, '09:00:00', 3, 3, 1, 250, 2),
+(11, 20, 3, '10:00:00', 9, 3, 1, 200, 2),
+(12, 80, 32, '16:00:00', 4, 3, 2, 300, 2),
+(13, 85, 40, '12:00:00', 2, 2, 1, 500, 2),
+(14, 150, 45, '18:00:00', 6, 1, 1, 190, 2),
+(15, 1150, 1000, '14:00:00', 8, 1, 20, 750, 2),
+(16, 12, 4, '13:00:00', 6, 1, 0, 150, 3),
+(17, 13, 6, '11:00:00', 8, 1, 1, 170, 3),
+(18, 80, 45, '12:00:00', 3, 1, 1, 100, 3),
+(19, 70, 50, '14:00:00', 1, 3, 2, 120, 3),
+(20, 150, 60, '13:00:00', 3, 1, 0, 710, 3),
+(21, 1200, 1100, '18:00:00', 10, 2, 10, 550, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -47,27 +72,27 @@ CREATE TABLE `data` (
 --
 
 CREATE TABLE `month` (
-  `id` int(11) NOT NULL,
-  `month` varchar(9) NOT NULL
+  `Id` int(11) NOT NULL,
+  `Month` varchar(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `month`
 --
 
-INSERT INTO `month` (`id`, `month`) VALUES
-(1, 'Januari'),
-(2, 'Februari'),
-(3, 'Maret'),
+INSERT INTO `month` (`Id`, `Month`) VALUES
+(1, 'January'),
+(2, 'February'),
+(3, 'March'),
 (4, 'April'),
-(5, 'Mei'),
-(6, 'Juni'),
-(7, 'Juli'),
-(8, 'Agustus'),
+(5, 'May'),
+(6, 'June'),
+(7, 'July'),
+(8, 'August'),
 (9, 'September'),
-(10, 'Oktober'),
+(10, 'October'),
 (11, 'November'),
-(12, 'Desember');
+(12, 'December');
 
 -- --------------------------------------------------------
 
@@ -76,7 +101,7 @@ INSERT INTO `month` (`id`, `month`) VALUES
 --
 
 CREATE TABLE `temp_indicator` (
-  `id` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `TempIndicator` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -84,7 +109,7 @@ CREATE TABLE `temp_indicator` (
 -- Dumping data for table `temp_indicator`
 --
 
-INSERT INTO `temp_indicator` (`id`, `TempIndicator`) VALUES
+INSERT INTO `temp_indicator` (`Id`, `TempIndicator`) VALUES
 (1, 'Cool'),
 (2, 'Mild'),
 (3, 'Hot');
@@ -96,15 +121,15 @@ INSERT INTO `temp_indicator` (`id`, `TempIndicator`) VALUES
 --
 
 CREATE TABLE `weather` (
-  `id` int(11) NOT NULL,
-  `weather` varchar(11) NOT NULL
+  `Id` int(11) NOT NULL,
+  `Weather` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `weather`
 --
 
-INSERT INTO `weather` (`id`, `weather`) VALUES
+INSERT INTO `weather` (`Id`, `Weather`) VALUES
 (1, 'Sunny'),
 (2, 'Rainy'),
 (3, 'Overcast');
@@ -117,7 +142,7 @@ INSERT INTO `weather` (`id`, `weather`) VALUES
 -- Indexes for table `data`
 --
 ALTER TABLE `data`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`Id`),
   ADD KEY `fk_month` (`Month`),
   ADD KEY `fk_weather` (`Weather`),
   ADD KEY `fk_temp_indicator` (`TempIndicator`);
@@ -126,19 +151,19 @@ ALTER TABLE `data`
 -- Indexes for table `month`
 --
 ALTER TABLE `month`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indexes for table `temp_indicator`
 --
 ALTER TABLE `temp_indicator`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indexes for table `weather`
 --
 ALTER TABLE `weather`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -148,25 +173,25 @@ ALTER TABLE `weather`
 -- AUTO_INCREMENT for table `data`
 --
 ALTER TABLE `data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `month`
 --
 ALTER TABLE `month`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `temp_indicator`
 --
 ALTER TABLE `temp_indicator`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `weather`
 --
 ALTER TABLE `weather`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -176,9 +201,9 @@ ALTER TABLE `weather`
 -- Constraints for table `data`
 --
 ALTER TABLE `data`
-  ADD CONSTRAINT `fk_month` FOREIGN KEY (`Month`) REFERENCES `month` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_temp_indicator` FOREIGN KEY (`TempIndicator`) REFERENCES `temp_indicator` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_weather` FOREIGN KEY (`Weather`) REFERENCES `weather` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_month` FOREIGN KEY (`Month`) REFERENCES `month` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_temp_indicator` FOREIGN KEY (`TempIndicator`) REFERENCES `temp_indicator` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_weather` FOREIGN KEY (`Weather`) REFERENCES `weather` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
